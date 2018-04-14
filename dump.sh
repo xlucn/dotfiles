4f4e2b785c9e4f2a2e7859bf2d566d3e15181dc3
Oliver Lew <oliver_lew@outlook.com>
2018-04-14 11:29:22 +0800

    add a function 'makelink' to create link file and improved control flow

--- dump.sh
+++ dump.sh
@@ -1,24 +1,31 @@
 #!/usr/bin/env sh
+
+makelink()
+{
+	mkdir -p $(dirname $2)
+	echo "Creating link $2 -> $1"
+	ln -sf $1 $2
+}
+
 link()
 {
 	if [ -e $2 ]
 	then
 		echo -e "The file/folder $2 exists already, do you want to replace it?\nBackup[b], No[n], Replace[R]"
 		read respond
+		if [ x$respond = "xb" ]
+		then
+			echo "Backing up $2 as $2.bak"
+			mv $2 $2.bak
+		fi
+		
 		if [ ! -e $2 -o x$respond = "xr" -o x$respond = "x" ]
+		then
+			makelink $PWD/$1 $2
+		fi			
+	else
+		makelink $PWD/$1 $2
 	fi
-	
-	if [ x$respond = "xb" ]
-	then
-		echo "Backing up $2 as $2.bak"
-		mv $2 $2.bak
-	fi
-	
-	then
-		mkdir -p $(dirname $2)
-		echo "Creating link $2 -> $1"
-		ln -sf $PWD/$1 $2
-	fi	
 }
 
 if [ x$1 = "xtest" ]
