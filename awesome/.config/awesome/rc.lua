f1d4c30d6ddd00eef3989522c0359cb043d3e5f5
Oliver Lew <oliver_lew@outlook.com>
2019-03-26 13:17:22 +0800

    try only show the current focused window

--- awesome/.config/awesome/rc.lua
+++ awesome/.config/awesome/rc.lua
@@ -542,7 +542,8 @@ awful.screen.connect_for_each_screen(function(s)
     s.mytaglist = awful.widget.taglist(s, awful.widget.taglist.filter.all, taglist_buttons)
 
     -- Create a tasklist widget
-    s.mytasklist = awful.widget.tasklist(s, awful.widget.tasklist.filter.currenttags, tasklist_buttons)
+    --s.mytasklist = awful.widget.tasklist(s, awful.widget.tasklist.filter.currenttags, tasklist_buttons)
+    s.mytasklist = awful.widget.tasklist(s, awful.widget.tasklist.filter.focused, tasklist_buttons)
 
     -- Create the wibox
     s.mywibox = awful.wibar({
