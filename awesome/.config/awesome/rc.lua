fd9c7b1b6a21b2b64542fe271d5be19ddad63658
Oliver Lew <oliver_lew@outlook.com>
2019-03-28 00:56:06 +0800

    don't show menu on right

--- awesome/.config/awesome/rc.lua
+++ awesome/.config/awesome/rc.lua
@@ -593,8 +593,7 @@ end)
 
 -- {{{ Mouse bindings
 root.buttons(gears.table.join(
-    awful.button({ }, 3, function () mymainmenu:toggle() end)--,
-    -- Don't switch views when scrolling on wallpaper
+    --awful.button({ }, 3, function () mymainmenu:toggle() end)--,
     --awful.button({ }, 4, awful.tag.viewnext),
     --awful.button({ }, 5, awful.tag.viewprev)
 ))
