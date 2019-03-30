5aafe79cadb89836f8b2356b6c95c6a6a365e754
Oliver Lew <oliver_lew@outlook.com>
2019-03-30 18:34:57 +0800

    commenting out float layout

--- awesome/.config/awesome/rc.lua
+++ awesome/.config/awesome/rc.lua
@@ -84,7 +84,7 @@ awful.layout.layouts = {
     awful.layout.suit.max,
     awful.layout.suit.magnifier,
     awful.layout.suit.corner.nw,
-    awful.layout.suit.floating,
+    --awful.layout.suit.floating,
     --awful.layout.suit.max.fullscreen,
     --awful.layout.suit.corner.ne,
     --awful.layout.suit.corner.sw,
