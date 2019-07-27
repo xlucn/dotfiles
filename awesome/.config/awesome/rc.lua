9baac6ff8cf178f17b17f303303526e20868270e
Oliver Lew <oliver_lew@outlook.com>
2019-07-27 21:37:08 +0800

    delete unused code

--- awesome/.config/awesome/rc.lua
+++ awesome/.config/awesome/rc.lua
@@ -1266,14 +1266,6 @@ client.connect_signal("request::titlebars", function(c)
     }
 end)
 
----- Enable sloppy focus, so that focus follows mouse.
---client.connect_signal("mouse::enter", function(c)
-    --if awful.layout.get(c.screen) ~= awful.layout.suit.magnifier
-        --and awful.client.focus.filter(c) then
-        --client.focus = c
-    --end
---end)
-
 client.connect_signal("focus", function(c) c.border_color = beautiful.border_focus end)
 client.connect_signal("unfocus", function(c) c.border_color = beautiful.border_normal end)
 -- }}}
