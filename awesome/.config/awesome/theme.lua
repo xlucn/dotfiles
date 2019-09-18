3d2311ec9175bdd9bd26d98ab65cf7411b8c62bf
Oliver Lew <oliver_lew@outlook.com>
2019-09-18 20:11:45 +0800

    simplify colors

--- awesome/.config/awesome/theme.lua
+++ awesome/.config/awesome/theme.lua
@@ -1,119 +1,81 @@
 local theme = {}
 
--- Gruvbox color table {{{
--- https://github.com/morhetz/gruvbox-contrib/blob/master/color.table
---    GRUVCOLR         "HEX    " -- RELATV ALIAS   TERMCOLOR      RGB           ITERM RGB     OSX HEX
--------------------    "-------" -- ------------   ------------   -----------   -----------   -------
-local dark0_hard     = "#1d2021" -- [   ]  [   ]   234 [h0][  ]    29- 32- 33    22- 24- 25   #161819
-local dark0          = "#282828" -- [bg0]  [fg0]   235 [ 0][  ]    40- 40- 40    30- 30- 30   #1e1e1e
-local dark0_soft     = "#32302f" -- [   ]  [   ]   236 [s0][  ]    50- 48- 47    38- 36- 35   #262423
-local dark1          = "#3c3836" -- [bg1]  [fg1]   237 [  ][15]    60- 56- 54    46- 42- 41   #2e2a29
-local dark2          = "#504945" -- [bg2]  [fg2]   239 [  ][  ]    80- 73- 69    63- 57- 53   #3f3935
-local dark3          = "#665c54" -- [bg3]  [fg3]   241 [  ][  ]   102- 92- 84    83- 74- 66   #534a42
-local dark4          = "#7c6f64" -- [bg4]  [fg4]   243 [  ][ 7]   124-111-100   104- 92- 81   #685c51
-
-local gray_245       = "#928374" -- [gray] [   ]   245 [ 8][  ]   146-131-116   127-112- 97   #7f7061
-local gray_244       = "#928374" -- [   ] [gray]   244 [  ][ 8]   146-131-116   127-112- 97   #7f7061
-
-local light0_hard    = "#f9f5d7" -- [   ]  [   ]   230 [  ][h0]   249-245-215   248-244-205   #f8f4cd
-local light0         = "#fbf1c7" -- [fg0]  [bg0]   229 [  ][ 0]   251-241-199   250-238-187   #faeebb
-local light0_soft    = "#f2e5bc" -- [   ]  [   ]   228 [  ][s0]   242-229-188   239-223-174   #efdfae
-local light1         = "#ebdbb2" -- [fg1]  [bg1]   223 [15][  ]   235-219-178   230-212-163   #e6d4a3
-local light2         = "#d5c4a1" -- [fg2]  [bg2]   250 [  ][  ]   213-196-161   203-184-144   #cbb890
-local light3         = "#bdae93" -- [fg3]  [bg3]   248 [  ][  ]   189-174-147   175-159-129   #af9f81
-local light4         = "#a89984" -- [fg4]  [bg4]   246 [ 7][  ]   168-153-132   151-135-113   #978771
-
-local bright_red     = "#fb4934" -- [red]   [  ]   167 [ 9][  ]   251- 73- 52   247- 48- 40   #f73028
-local bright_green   = "#b8bb26" -- [green] [  ]   142 [10][  ]   184-187- 38   170-176- 30   #aab01e
-local bright_yellow  = "#fabd2f" -- [yellow][  ]   214 [11][  ]   250-189- 47   247-177- 37   #f7b125
-local bright_blue    = "#83a598" -- [blue]  [  ]   109 [12][  ]   131-165-152   113-149-134   #719586
-local bright_purple  = "#d3869b" -- [purple][  ]   175 [13][  ]   211-134-155   199-112-137   #c77089
-local bright_aqua    = "#8ec07c" -- [aqua]  [  ]   108 [14][  ]   142-192-124   125-182-105   #7db669
-local bright_orange  = "#fe8019" -- [orange][  ]   208 [  ][  ]   254-128- 25   251-106- 22   #fb6a16
-
-local neutral_red    = "#cc241d" -- [   ]  [   ]   124 [ 1][ 1]   204- 36- 29   190- 15- 23   #be0f17
-local neutral_green  = "#98971a" -- [   ]  [   ]   106 [ 2][ 2]   152-151- 26   134-135- 21   #868715
-local neutral_yellow = "#d79921" -- [   ]  [   ]   172 [ 3][ 3]   215-153- 33   204-136- 26   #cc881a
-local neutral_blue   = "#458588" -- [   ]  [   ]    66 [ 4][ 4]    69-133-136    55-115-117   #377375
-local neutral_purple = "#b16286" -- [   ]  [   ]   132 [ 5][ 5]   177- 98-134   160- 75-115   #a04b73
-local neutral_aqua   = "#689d6a" -- [   ]  [   ]    72 [ 6][ 6]   104-157-106    87-142- 87   #578e57
-local neutral_orange = "#d65d0e" -- [   ]  [   ]   166 [  ][  ]   214- 93- 14   202- 72- 14   #ca480e
-
-local faded_red      = "#9d0006" -- [   ]   [red]   88 [  ][ 9]   157-  0-  6   137-  0-  9   #890009
-local faded_green    = "#79740e" -- [   ] [green]  100 [  ][10]   121-116- 14   102- 98- 13   #66620d
-local faded_yellow   = "#b57614" -- [   ][yellow]  136 [  ][11]   181-118- 20   165- 99- 17   #a56311
-local faded_blue     = "#076678" -- [   ]  [blue]   24 [  ][12]     7-102-120    14- 83-101   #0e5365
-local faded_purple   = "#8f3f71" -- [   ][purple]   96 [  ][13]   143- 63-113   123- 43- 94   #7b2b5e
-local faded_aqua     = "#427b58" -- [   ]  [aqua]   66 [  ][14]    66-123- 88    53-106- 70   #356a46
-local faded_orange   = "#af3a03" -- [   ][orange]  130 [  ][  ]   175- 58- 3    157- 40- 7    #9d2807
--- }}}
-
 -- Libraries {{{
 local gears = require("gears")
 local themes_path = gears.filesystem.get_themes_dir()
-local config_path = gears.filesystem.get_configuration_dir()
 local dpi = require("beautiful.xresources").apply_dpi
 local theme_assets = require("beautiful.theme_assets")
 -- }}}
 
+-- Gruvbox color table {{{
+-- https://github.com/morhetz/gruvbox-contrib/blob/master/color.table
+-- GRUVCOLR     "HEX    " --  ALIAS     RGB
+-------------   "-------" -- --------   -----------
+local dark0   = "#282828" -- [bg0]       40- 40- 40
+local dark1   = "#3c3836" -- [bg1]       60- 56- 54
+local dark2   = "#504945" -- [bg2]       80- 73- 69
+local dark3   = "#665c54" -- [bg3]      102- 92- 84
+local dark4   = "#7c6f64" -- [bg4]      124-111-100
+
+local light0  = "#fbf1c7" -- [fg0]      251-241-199
+
+local red     = "#fb4934" -- [red]      251- 73- 52
+local green   = "#b8bb26" -- [green]    184-187- 38
+local yellow  = "#fabd2f" -- [yellow]   250-189- 47
+local blue    = "#83a598" -- [blue]     131-165-152
+local purple  = "#d3869b" -- [purple]   211-134-155
+local aqua    = "#8ec07c" -- [aqua]     142-192-124
+local orange  = "#fe8019" -- [orange]   254-128- 25
+-- }}}
+
 -- Basic Colors {{{
-theme.blue                  = neutral_blue
-theme.red                   = neutral_red
 theme.bg_normal             = dark0
-theme.bg_focus              = dark2 -- neutral_blue
-theme.bg_urgent             = bright_purple
+theme.bg_focus              = dark2
+theme.bg_urgent             = purple
 theme.bg_minimize           = theme.bg_normal
 
 theme.fg_normal             = light0
-theme.fg_focus              = neutral_blue
+theme.fg_focus              = blue
 theme.fg_urgent             = theme.bg_normal
-theme.fg_minimize           = gray_244
+theme.fg_minimize           = dark4
 -- }}}
 
 -- wallpaper {{{
 theme.wallpaper = "~/.local/share/backgrounds/wallpaper.jpg"
---theme.wallpaper = themes_path .. "zenburn/zenburn-background.png"
---gears.wallpaper.set(dark2)
 -- }}}
 
 -- Colors {{{
 theme.border_normal         = theme.bg_normal
-theme.border_focus          = neutral_yellow
-theme.border_marked         = neutral_red
+theme.border_focus          = yellow
+theme.border_marked         = red
 theme.titlebar_bg_focus     = dark3
 -- }}}
 
 -- Widget colors {{{
-theme.widget_music          = bright_purple
-theme.widget_light          = bright_yellow
-theme.widget_ram            = bright_blue
+theme.widget_music          = purple
+theme.widget_light          = yellow
+theme.widget_ram            = blue
 theme.widget_ram_high       = birght_red
-theme.widget_cpu            = bright_blue
-theme.widget_cpu_high       = bright_red
-theme.widget_alsa           = bright_yellow
-theme.widget_bat_normal     = bright_green
-theme.widget_bat_mid        = bright_green
-theme.widget_bat_low        = bright_orange
-theme.widget_bat_empty      = bright_red
-theme.widget_mail_online    = bright_green
+theme.widget_cpu            = blue
+theme.widget_cpu_high       = red
+theme.widget_alsa           = yellow
+theme.widget_bat_normal     = green
+theme.widget_bat_mid        = green
+theme.widget_bat_low        = orange
+theme.widget_bat_empty      = red
+theme.widget_mail_online    = green
 theme.widget_mail_offline   = theme.fg_normal
 -- }}}
 
 -- Taglist {{{
-theme.taglist_font          = "Hack Nerd Font 14"
-theme.taglist_squares_sel   = nil
-theme.taglist_squares_unsel = nil
-theme.taglist_fg_empty      = theme.fg_normal
-theme.taglist_bg_empty      = theme.bg_normal -- dark1
-theme.taglist_fg_occupied   = neutral_orange -- theme.fg_focus
-theme.taglist_bg_occupied   = theme.bg_normal -- dark1
-theme.taglist_fg_focus      = neutral_orange -- theme.fg_focus
-theme.taglist_bg_focus      = faded_blue
+theme.taglist_font                = "Hack Nerd Font 14"
+theme.taglist_squares_sel         = gears.surface.load_from_shape (4, 4, gears.shape.rectangle, red)
+theme.taglist_squares_unsel       = gears.surface.load_from_shape (4, 4, gears.shape.rectangle, red)
 -- }}}
 
 -- Tasklist {{{
 theme.tasklist_fg_focus     = theme.fg_focus
-theme.tasklist_bg_focus     = neutral_aqua
+theme.tasklist_bg_focus     = blue
 theme.tasklist_bg_minimize  = dark1
 theme.tasklist_spacing      = dpi(0)
 -- }}}
@@ -259,36 +221,39 @@ theme.layout_cornerse   = themes_path .. "zenburn/layouts/cornerse.png"
 -- }}}
 
 -- {{{ Titlebar
-theme.titlebar_close_button_focus  = gears.surface.load_from_shape (24, 12, gears.shape.rectangle, bright_red)
-theme.titlebar_close_button_normal = gears.surface.load_from_shape (24, 12, gears.shape.rectangle, bright_red)
+titlebar_button_height = 12
+titlebar_button_width = titlebar_button_height * 2
 
-theme.titlebar_maximized_button_focus_active    = gears.surface.load_from_shape (24, 12, gears.shape.rectangle, bright_green)
-theme.titlebar_maximized_button_normal_active   = gears.surface.load_from_shape (24, 12, gears.shape.rectangle, bright_green)
-theme.titlebar_maximized_button_focus_inactive  = gears.surface.load_from_shape (24, 12, gears.shape.rectangle, bright_green)
-theme.titlebar_maximized_button_normal_inactive = gears.surface.load_from_shape (24, 12, gears.shape.rectangle, bright_green)
+theme.titlebar_close_button_focus  = gears.surface.load_from_shape (titlebar_button_width, titlebar_button_height, gears.shape.rectangle, red)
+theme.titlebar_close_button_normal = gears.surface.load_from_shape (titlebar_button_width, titlebar_button_height, gears.shape.rectangle, theme.bg_focus)
 
-theme.titlebar_minimize_button_focus  = gears.surface.load_from_shape (24, 12, gears.shape.rectangle, bright_yellow)
-theme.titlebar_minimize_button_normal = gears.surface.load_from_shape (24, 12, gears.shape.rectangle, bright_yellow)
+theme.titlebar_maximized_button_focus_active    = gears.surface.load_from_shape (titlebar_button_width, titlebar_button_height, gears.shape.rectangle, green)
+theme.titlebar_maximized_button_normal_active   = gears.surface.load_from_shape (titlebar_button_width, titlebar_button_height, gears.shape.rectangle, theme.bg_focus)
+theme.titlebar_maximized_button_focus_inactive  = gears.surface.load_from_shape (titlebar_button_width, titlebar_button_height, gears.shape.rectangle, green)
+theme.titlebar_maximized_button_normal_inactive = gears.surface.load_from_shape (titlebar_button_width, titlebar_button_height, gears.shape.rectangle, theme.bg_focus)
 
-theme.titlebar_ontop_button_focus_active    = gears.surface.load_from_shape (24, 12, gears.shape.rectangle, bright_blue)
-theme.titlebar_ontop_button_normal_active   = gears.surface.load_from_shape (24, 12, gears.shape.rectangle, bright_blue)
-theme.titlebar_ontop_button_focus_inactive  = gears.surface.load_from_shape (24, 12, gears.shape.rectangle, bright_blue)
-theme.titlebar_ontop_button_normal_inactive = gears.surface.load_from_shape (24, 12, gears.shape.rectangle, bright_blue)
+theme.titlebar_minimize_button_focus  = gears.surface.load_from_shape (titlebar_button_width, titlebar_button_height, gears.shape.rectangle, yellow)
+theme.titlebar_minimize_button_normal = gears.surface.load_from_shape (titlebar_button_width, titlebar_button_height, gears.shape.rectangle, theme.bg_focus)
 
-theme.titlebar_sticky_button_focus_active    = gears.surface.load_from_shape (24, 12, gears.shape.rectangle, bright_purple)
-theme.titlebar_sticky_button_normal_active   = gears.surface.load_from_shape (24, 12, gears.shape.rectangle, bright_purple)
-theme.titlebar_sticky_button_focus_inactive  = gears.surface.load_from_shape (24, 12, gears.shape.rectangle, bright_purple)
-theme.titlebar_sticky_button_normal_inactive = gears.surface.load_from_shape (24, 12, gears.shape.rectangle, bright_purple)
+theme.titlebar_ontop_button_focus_active    = gears.surface.load_from_shape (titlebar_button_width, titlebar_button_height, gears.shape.rectangle, blue)
+theme.titlebar_ontop_button_normal_active   = gears.surface.load_from_shape (titlebar_button_width, titlebar_button_height, gears.shape.rectangle, theme.bg_focus)
+theme.titlebar_ontop_button_focus_inactive  = gears.surface.load_from_shape (titlebar_button_width, titlebar_button_height, gears.shape.rectangle, blue)
+theme.titlebar_ontop_button_normal_inactive = gears.surface.load_from_shape (titlebar_button_width, titlebar_button_height, gears.shape.rectangle, theme.bg_focus)
 
-theme.titlebar_floating_button_focus_active    = gears.surface.load_from_shape (24, 12, gears.shape.rectangle, bright_aqua)
-theme.titlebar_floating_button_normal_active   = gears.surface.load_from_shape (24, 12, gears.shape.rectangle, bright_aqua)
-theme.titlebar_floating_button_focus_inactive  = gears.surface.load_from_shape (24, 12, gears.shape.rectangle, bright_aqua)
-theme.titlebar_floating_button_normal_inactive = gears.surface.load_from_shape (24, 12, gears.shape.rectangle, bright_aqua)
+theme.titlebar_sticky_button_focus_active    = gears.surface.load_from_shape (titlebar_button_width, titlebar_button_height, gears.shape.rectangle, purple)
+theme.titlebar_sticky_button_normal_active   = gears.surface.load_from_shape (titlebar_button_width, titlebar_button_height, gears.shape.rectangle, theme.bg_focus)
+theme.titlebar_sticky_button_focus_inactive  = gears.surface.load_from_shape (titlebar_button_width, titlebar_button_height, gears.shape.rectangle, purple)
+theme.titlebar_sticky_button_normal_inactive = gears.surface.load_from_shape (titlebar_button_width, titlebar_button_height, gears.shape.rectangle, theme.bg_focus)
+
+theme.titlebar_floating_button_focus_active    = gears.surface.load_from_shape (titlebar_button_width, titlebar_button_height, gears.shape.rectangle, aqua)
+theme.titlebar_floating_button_normal_active   = gears.surface.load_from_shape (titlebar_button_width, titlebar_button_height, gears.shape.rectangle, theme.bg_focus)
+theme.titlebar_floating_button_focus_inactive  = gears.surface.load_from_shape (titlebar_button_width, titlebar_button_height, gears.shape.rectangle, aqua)
+theme.titlebar_floating_button_normal_inactive = gears.surface.load_from_shape (titlebar_button_width, titlebar_button_height, gears.shape.rectangle, theme.bg_focus)
 -- }}}
 
 -- Generate Awesome icon:
 theme.awesome_icon = theme_assets.awesome_icon(
-    theme.menu_height, bright_blue, theme.bg_normal
+    theme.menu_height, blue, theme.bg_normal
 )
 
 -- Icon theme
