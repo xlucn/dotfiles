556526528c8ffc8c29925336eda5207225bd08da
Lu Xu <oliver_lew@outlook.com>
2021-12-26 16:45:23 +0800

    irssi: autorun stop verbose messages (irssi does already)

--- irssi/.config/irssi/scripts/autorun/autorun.pl
+++ irssi/.config/irssi/scripts/autorun/autorun.pl
@@ -4,8 +4,7 @@ use Irssi;
 
 # Specify autorun scripts through this script. This way, it is easier to
 # manage dotfiles syncing since it's text instead of symlinks.
-
-my @scripts = (
+foreach my $s (
     '/usr/share/irssi/scripts/adv_windowlist.pl',
     '/usr/share/irssi/scripts/chansort_configurable.pl',
     '/usr/share/irssi/scripts/mouse.pl',
@@ -13,9 +12,6 @@ my @scripts = (
     '/usr/share/irssi/scripts/tmux-nicklist-portable.pl',
     '/usr/share/irssi/scripts/trackbar.pl',
     '/usr/share/irssi/scripts/usercount.pl',
-);
-
-foreach my $s (@scripts) {
+) {
     Irssi::command("script load $s");
-    print "Script $s loaded.";
 }
