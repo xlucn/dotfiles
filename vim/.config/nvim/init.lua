c8bb2f56ef107fc9488f4b32e56f4b9d33842988
Lu Xu <oliver_lew@outlook.com>
2022-06-21 20:02:00 +0800

    vim: nvim lsp status improvement, and keybinding

--- vim/.config/nvim/init.lua
+++ vim/.config/nvim/init.lua
@@ -32,14 +32,22 @@ vim.cmd('packadd cmp-vsnip')
 vim.cmd('packadd vim-vsnip')
 vim.cmd('packadd vim-vsnip-integ')
 
-local opts = { noremap = true, silent = true }
-vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, opts)
-vim.keymap.set('n', ']d', vim.diagnostic.goto_next, opts)
-vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
-vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
-vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
-vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
-vim.keymap.set('i', '<C-k>', vim.lsp.buf.signature_help, opts)
+function LSPStatus()
+    local total, count = 0, 0
+    local message = ' '
+    local levels = { E = 'ERROR', W = 'WARN', I = 'INFO', H = 'HINT' }
+    for k, v in pairs(levels) do
+        count = #vim.diagnostic.get(0, { severity = vim.diagnostic.severity[v] })
+        if count > 0 then
+            message = message .. k .. ': ' .. count .. ' '
+            total = total + count
+        end
+    end
+    if total == 0 then
+        message = message .. 'OK '
+    end
+    return message
+end
 
 local capabilities = vim.lsp.protocol.make_client_capabilities()
 capabilities = require('cmp_nvim_lsp').update_capabilities(capabilities)
@@ -55,7 +63,7 @@ cmp.setup({
     mapping = cmp.mapping.preset.insert({
       ['<C-b>'] = cmp.mapping.scroll_docs(-4),
       ['<C-f>'] = cmp.mapping.scroll_docs(4),
-      ['<CR>'] = cmp.mapping.confirm({ select = true }),
+      ['<CR>'] = cmp.mapping.confirm({ select = false }),
     }),
     sources = cmp.config.sources({
       { name = 'nvim_lsp' },
@@ -65,17 +73,41 @@ cmp.setup({
     })
 })
 
+-- servers with simple setup
 local servers = {
     'clangd',
     'pylsp',
     'bashls',
 }
+local on_attach = function(_, bufnr)
+    -- Enable completion triggered by <c-x><c-o>
+    vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
+
+    -- Mappings.
+    -- See `:help vim.lsp.*` for documentation on any of the below functions
+    local bufopts = { noremap=true, silent=true, buffer=bufnr }
+    vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, bufopts)
+    vim.keymap.set('n', ']d', vim.diagnostic.goto_next, bufopts)
+    vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, bufopts)
+    vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts)
+    vim.keymap.set('n', 'K', vim.lsp.buf.hover, bufopts)
+    vim.keymap.set('i', '<C-k>', vim.lsp.buf.signature_help, bufopts)
+    vim.keymap.set('n', ',li', vim.lsp.buf.implementation, bufopts)
+    vim.keymap.set('n', ',lr', vim.lsp.buf.references, bufopts)
+    vim.keymap.set('n', ',lR', vim.lsp.buf.rename, bufopts)
+    vim.keymap.set('n', ',la', vim.lsp.buf.code_action, bufopts)
+    vim.keymap.set('n', ',lf', vim.lsp.buf.formatting, bufopts)
+end
 for _, server in ipairs(servers) do
-    lsp[server].setup({ capabilities = capabilities })
+    lsp[server].setup({
+        capabilities = capabilities,
+        on_attach = on_attach,
+    })
 end
 
 lsp['sumneko_lua'].setup({
     capabilities = capabilities,
+    on_attach = on_attach,
     settings = {
         Lua = {
             diagnostics = {
@@ -86,6 +118,7 @@ lsp['sumneko_lua'].setup({
 })
 lsp['texlab'].setup({
     capabilities = capabilities,
+    on_attach = on_attach,
     settings = {
         texlab = {
             build = {
