746067a5982aa03b3d02748c09af6443defec9f6
Lu Xu <oliver_lew@outlook.com>
2022-06-30 19:49:01 +0800

    vim: put package managing all into vimrc

--- vim/.config/nvim/init.lua
+++ vim/.config/nvim/init.lua
@@ -1,38 +1,4 @@
-vim.cmd('packadd vim-packager')
-require('packager').setup(function(packager)
-    -- Package manager
-    packager.add('kristijanhusak/vim-packager', {type = 'opt'})
-    -- Basic
-    packager.add('tpope/vim-commentary')
-    packager.add('tpope/vim-endwise')
-    packager.add('tpope/vim-repeat')
-    packager.add('tpope/vim-sensible')
-    packager.add('tpope/vim-surround')
-    packager.add('tpope/vim-dispatch')
-    packager.add('tpope/vim-unimpaired')
-    packager.add('ap/vim-buftabline')
-    -- Language server
-    packager.add('neovim/nvim-lspconfig', {type = 'opt'});
-    packager.add('hrsh7th/nvim-cmp', {type = 'opt'});
-    packager.add('hrsh7th/cmp-nvim-lsp', {type = 'opt'});
-    packager.add('hrsh7th/cmp-vsnip', {type = 'opt'});
-    -- Enhancements
-    packager.add('nvim-lua/plenary.nvim')
-    packager.add('nvim-telescope/telescope.nvim')
-    packager.add('majutsushi/tagbar')
-    packager.add('liuchengxu/vim-which-key')
-    packager.add('jpalardy/vim-slime')
-    packager.add('airblade/vim-gitgutter')
-end)
-
-vim.cmd('packadd nvim-lspconfig')
-vim.cmd('packadd cmp-nvim-lsp')
-vim.cmd('packadd nvim-cmp')
-vim.cmd('packadd cmp-vsnip')
-vim.cmd('packadd vim-vsnip')
-vim.cmd('packadd vim-vsnip-integ')
-
-function LSPStatus()
+function NvimLSPStatus()
     local total, count = 0, 0
     local message = ' '
     local levels = { E = 'ERROR', W = 'WARN', I = 'INFO', H = 'HINT' }
@@ -142,3 +108,8 @@ lsp['texlab'].setup({
         }
     }
 })
+
+local whichkey = require("which-key")
+whichkey.register({
+    l = { name = "language server" }
+}, { prefix = "<leader>" })
