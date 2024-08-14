607819f4ea0a397e998c6fbd78baf4c75ad2c3a1
Lu Xu <oliver_lew@outlook.com>
2024-08-14 15:42:29 +0800

    nvim: add more dap configurations

--- vim/.config/nvim/lua/plugins/dap.lua
+++ vim/.config/nvim/lua/plugins/dap.lua
@@ -1,11 +1,13 @@
+local prompt_inputfile = function()
+    return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
+end
+
 return {
     {
         'rcarriga/nvim-dap-ui',
         dependencies = {
             { 'mfussenegger/nvim-dap' },
-            { 'mfussenegger/nvim-dap-python', config = function()
-                require('dap-python').setup()
-            end },
+            { 'mfussenegger/nvim-dap-python' },
             { "nvim-neotest/nvim-nio" },
         },
         config = true,
@@ -14,5 +16,53 @@ return {
                 require'dapui'.toggle()
             end, mode = 'n' }
         },
+    },
+    {
+        'mfussenegger/nvim-dap-python',
+        config = function()
+            require('dap-python').setup()
+            -- Allow step into library code
+            require('dap').configurations.python[1].justMyCode = false
+        end
+    },
+    {
+        'mfussenegger/nvim-dap',
+        dependencies = {
+            { 'mfussenegger/nvim-dap-python' }
+        },
+        config = function()
+            local dap = require('dap')
+            dap.adapters.cppdbg = {
+                id = 'cppdbg',
+                type = 'executable',
+                command = 'cppdbg',  -- packaged in AUR
+            }
+            dap.adapters.lldb = {
+                type = 'executable',
+                command = '/usr/bin/lldb-dap',
+                name = "lldb",
+            }
+            dap.configurations.cpp = {
+                {
+                    name = "Launch file (cppdbg)",
+                    type = "cppdbg",
+                    request = "launch",
+                    program = prompt_inputfile,
+                    cwd = '${workspaceFolder}',
+                    stopAtEntry = true,
+                },
+                {
+                    name = "Launch file (lldb)",
+                    type = "lldb",
+                    request = "launch",
+                    program = prompt_inputfile,
+                    cwd = '${workspaceFolder}',
+                    stopOnEntry = false,
+                    args = {},
+                },
+            }
+            dap.configurations.c = dap.configurations.cpp
+            dap.configurations.rust = dap.configurations.cpp
+        end,
     },
 }
