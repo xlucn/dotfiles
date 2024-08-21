aa670d4ce9df36ed9516bf91e5e833901ee72209
Lu Xu <oliver_lew@outlook.com>
2024-08-21 23:55:25 +0800

    nvim: add render-markdown plugin

--- vim/.config/nvim/lua/plugins/filetype.lua
+++ vim/.config/nvim/lua/plugins/filetype.lua
@@ -3,4 +3,23 @@ return {
         'voldikss/vim-mma',
         ft = 'mma'
     },
+    {
+        "MeanderingProgrammer/render-markdown.nvim",
+        ft = "markdown",
+        init = function ()
+            vim.api.nvim_set_hl(0, "RenderMarkdownCode", { ctermbg = 0 })
+            vim.api.nvim_set_hl(0, "RenderMarkdownCodeInline", { ctermbg = 'none' })
+        end,
+        opts = {
+            latex = { enabled = false },
+            code = {
+                position = "right",
+                min_width = 50,
+            },
+        },
+        dependencies = {
+            'nvim-treesitter/nvim-treesitter',
+            'nvim-tree/nvim-web-devicons'
+        },
+    },
 }
