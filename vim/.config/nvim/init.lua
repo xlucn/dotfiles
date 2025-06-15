5da341e67f4851c816030a5795aa633b2aba8abb
Lu Xu <oliver_lew@outlook.com>
2025-06-15 16:16:39 +0800

    neovim: disable optional providers

--- vim/.config/nvim/init.lua
+++ vim/.config/nvim/init.lua
@@ -9,6 +9,10 @@ vim.o.mousemodel = 'extend'
 vim.o.mousemoveevent = true
 vim.o.termguicolors = false
 vim.g.health = { style = 'float' }
+vim.g.loaded_python3_provider = 0
+vim.g.loaded_perl_provider = 0
+vim.g.loaded_node_provider = 0
+vim.g.loaded_ruby_provider = 0
 
 -- color scheme
 local highlights = {
