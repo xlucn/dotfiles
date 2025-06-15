02a2a1d4604416ef8ce5154df3aea6f1014cc398
Lu Xu <oliver_lew@outlook.com>
2025-06-15 15:50:03 +0800

    neovim: render-markdown.nvim -> live-preview.nvim

--- vim/.config/nvim/lua/plugins/filetype.lua
+++ vim/.config/nvim/lua/plugins/filetype.lua
@@ -4,33 +4,10 @@ return {
         ft = 'mma'
     },
     {
-        "MeanderingProgrammer/render-markdown.nvim",
-        ft = "markdown",
-        init = function ()
-            vim.api.nvim_set_hl(0, "RenderMarkdownCode", { ctermbg = 0 })
-            vim.api.nvim_set_hl(0, "RenderMarkdownCodeInline", { ctermbg = 'none' })
-            vim.api.nvim_set_hl(0, "RenderMarkdownTableFill", { ctermbg = 'none' })
-        end,
-        opts = {
-            latex = { enabled = false },
-            heading = {
-                enabled = false,
-                -- position = 'inline',
-                border = true,
-                border_virtual = true,
-                -- border_prefix = true,
-            },
-            code = {
-                position = "right",
-                min_width = 72,
-            },
-            indent = {
-                enabled = false,
-            },
-        },
+        'brianhuster/live-preview.nvim',
+        ft = 'markdown',
         dependencies = {
-            'nvim-treesitter/nvim-treesitter',
-            'nvim-tree/nvim-web-devicons'
+            'nvim-telescope/telescope.nvim',
         },
-    },
+    }
 }
