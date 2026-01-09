27d82af7eecc232e8e1ae80eb6e55a0cc5b5d207
Lu Xu <oliver_lew@outlook.com>
2026-01-09 12:33:02 +0800

    nvim: add mason plugin

---
+++ vim/.config/nvim/lua/plugins/mason.lua
@@ -0,0 +1,13 @@
+return {
+    "mason-org/mason.nvim",
+    lazy = false,
+    opts = {
+        ui = {
+            icons = {
+                package_installed = "✓",
+                package_pending = "➜",
+                package_uninstalled = "✗"
+            }
+        }
+    },
+}
