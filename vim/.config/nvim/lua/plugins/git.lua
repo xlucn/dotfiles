e2e797539487f645e62723b95802b27a4a044333
Lu Xu <oliver_lew@outlook.com>
2026-03-19 14:59:23 +0800

    neovim: remove neogit

--- vim/.config/nvim/lua/plugins/git.lua
+++ vim/.config/nvim/lua/plugins/git.lua
@@ -1,17 +1,5 @@
 return {
     {
-        'NeogitOrg/neogit',
-        dependencies = {
-            'nvim-lua/plenary.nvim',
-        },
-        opts = {
-            kind = 'split',
-        },
-        keys = {
-            { "<leader>gg", "<cmd>Neogit<cr>", "n" },
-        },
-    },
-    {
         'lewis6991/gitsigns.nvim',
         config = true,
     },
