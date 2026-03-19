b5ae8ff053cabc198db9b04087b8ec911934d04c
Lu Xu <oliver_lew@outlook.com>
2026-03-19 14:57:12 +0800

    neovim: replace bufferline with simple lualine's tabline

--- vim/.config/nvim/lua/plugins/bufferline.lua
+++ vim/.config/nvim/lua/plugins/bufferline.lua
@@ -1,35 +1,5 @@
 return {
     {
-        'akinsho/bufferline.nvim',
-        dependencies = {
-            'nvim-tree/nvim-web-devicons',
-        },
-        opts = {
-            options = {
-                numbers = "ordinal",
-                right_mouse_command = false,
-                indicator = { style = 'none' },
-                color_icons = false,
-                show_buffer_icons = false,
-                max_name_length = 30,
-                separator_style = {'|', '|'},
-                hover = {
-                    enabled = true,
-                    delay = 0,
-                    reveal = {'close'}
-                },
-                offsets = {
-                    {
-                        filetype = "NvimTree",
-                        text = "File Explorer",
-                        text_align = "center",
-                        separator = true,
-                    }
-                },
-            },
-        },
-    },
-    {
         'ojroques/nvim-bufdel',
         config = true,
         keys = {
--- vim/.config/nvim/lua/plugins/lualine.lua
+++ vim/.config/nvim/lua/plugins/lualine.lua
@@ -6,6 +6,15 @@ return {
         },
         opts = {
             options = {
+                always_show_tabline = true,
+            },
+            tabline = {
+                lualine_a = {'buffers'},
+                lualine_b = {},
+                lualine_c = {},
+                lualine_x = {'searchcount'},
+                lualine_y = {'lsp_status'},
+                lualine_z = {'tabs'},
             },
         },
     }
