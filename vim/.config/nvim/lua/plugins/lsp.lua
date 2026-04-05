c28e84094988f347c70c2a514b94fef11459f737
Lu Xu <oliver_lew@outlook.com>
2026-04-06 01:42:07 +0800

    neovim: merge plugin spec files

--- vim/.config/nvim/lua/plugins/lsp.lua
+++ vim/.config/nvim/lua/plugins/lsp.lua
@@ -1,4 +1,30 @@
 return {
-    'neovim/nvim-lspconfig',
-    lazy = false,
+    {
+        'neovim/nvim-lspconfig',
+        lazy = false,
+    },
+    {
+        "mason-org/mason.nvim",
+        opts = {
+            ui = {
+                icons = {
+                    package_installed = "✓",
+                    package_pending = "➜",
+                    package_uninstalled = "✗"
+                }
+            }
+        }
+    },
+    {
+        "mason-org/mason-lspconfig.nvim",
+        opts = {
+            automatic_enable = {
+                exclude = { 'copilot', 'zuban' }
+            }
+        },
+        dependencies = {
+            "mason-org/mason.nvim",
+            "neovim/nvim-lspconfig",
+        },
+    }
 }
--- vim/.config/nvim/lua/plugins/mason.lua
+++ vim/.config/nvim/lua/plugins/mason.lua
@@ -1,26 +1,2 @@
 return {
-    {
-        "mason-org/mason.nvim",
-        opts = {
-            ui = {
-                icons = {
-                    package_installed = "✓",
-                    package_pending = "➜",
-                    package_uninstalled = "✗"
-                }
-            }
-        }
-    },
-    {
-        "mason-org/mason-lspconfig.nvim",
-        opts = {
-            automatic_enable = {
-                exclude = { 'copilot' }
-            }
-        },
-        dependencies = {
-            "mason-org/mason.nvim",
-            "neovim/nvim-lspconfig",
-        },
-    }
 }
