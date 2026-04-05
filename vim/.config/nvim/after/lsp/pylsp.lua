e8f6ba59bd68b1e7990fd1eabed45a11f5f99fab
Lu Xu <oliver_lew@outlook.com>
2026-04-06 01:28:24 +0800

    neovim: fix pylsp not completing numpy import

--- vim/.config/nvim/after/lsp/pylsp.lua
+++ vim/.config/nvim/after/lsp/pylsp.lua
@@ -3,7 +3,10 @@ return {
         pylsp = {
             plugins = {
                 jedi = {
+                    -- fix finding modules in virtualenvs
                     environment = vim.fn.exepath('python'),
+                    -- fix numpy imports, default was { 'numpy' }
+                    auto_import_modules = {},
                 },
             }
         }
