89aea11474d65e4457b70e8bde9c2f4d4dcc8a07
Lu Xu <oliver_lew@outlook.com>
2026-04-15 23:07:07 +0800

    neovim(blink.cmp): enable signature and cmdline

--- vim/.config/nvim/lua/plugins/complete.lua
+++ vim/.config/nvim/lua/plugins/complete.lua
@@ -1,7 +1,7 @@
 return {
     {
         'saghen/blink.cmp',
-        event = { 'InsertEnter' },
+        event = { 'InsertEnter', 'CmdlineEnter' },
         -- optional: provides snippets for the snippet source
         dependencies = {
             'rafamadriz/friendly-snippets',
@@ -12,36 +12,51 @@ return {
         version = '*',
 
         opts = {
-            -- 'default' for mappings similar to built-in completion
-            -- 'super-tab' for mappings similar to vscode (tab to accept, arrow keys to navigate)
-            -- 'enter' for mappings similar to 'super-tab' but with 'enter' to accept
-            -- see the "default configuration" section below for full documentation on how to define
-            -- your own keymap.
             keymap = {
                 preset = 'enter',
-                -- ['<C-g>'] = { 'hide', 'fallback' },
-                -- ['<C-y>'] = { 'accept', 'fallback' },
+                ['<C-c>'] = { 'hide', 'fallback' },
+                ['<Enter>'] = { 'accept', 'fallback' },
                 ['<C-p>'] = { 'snippet_backward', 'fallback' },
                 ['<C-n>'] = { 'snippet_forward', 'fallback' },
                 ['<Tab>'] = { 'select_next', 'fallback' },
                 ['<S-Tab>'] = { 'select_prev', 'fallback' },
             },
-
             completion = {
-                list = { selection = { preselect = false, auto_insert = true } },
-                documentation = { auto_show = true, },
-                menu = { draw = { treesitter = { 'lsp' }, } },
+                keyword = { range = 'full' },
+                list = { selection = {
+                    preselect = false,
+                    auto_insert = true,
+                } },
+                documentation = { auto_show = true },
+                menu = { draw = { treesitter = { 'lsp' } } },
             },
-
             sources = {
-                default = { "lazydev", "lsp", "path", "snippets", "buffer" },
+                default = { "lsp", "buffer", "path", "snippets" },
+                per_filetype = { lua = {
+                    inherit_defaults = true, 'lazydev'
+                } },
                 providers = {
                     lazydev = { module = "lazydev.integrations.blink" },
+                    path = { opts = {
+                        show_hidden_files_by_default = true,
+                    }}
                 },
             },
             cmdline = {
-                enabled = false,
-            }
+                enabled = true,
+                keymap = { preset = 'inherit' },
+                completion = {
+                    menu = { auto_show = true },
+                    list = { selection = {
+                        preselect = false,
+                        auto_insert = false,
+                    } }
+                },
+            },
+            signature = {
+                enabled = true,
+                trigger = { show_on_insert = true },
+            },
         },
     },
 }
