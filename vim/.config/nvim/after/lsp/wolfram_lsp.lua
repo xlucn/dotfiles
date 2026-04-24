c504b8ff323adce245e3c3a426f5c77897c260b7
Lu Xu <oliver_lew@outlook.com>
2026-04-24 14:44:14 +0800

    neovim: wolfram_lsp enable semanticTokens

--- vim/.config/nvim/after/lsp/wolfram_lsp.lua
+++ vim/.config/nvim/after/lsp/wolfram_lsp.lua
@@ -13,4 +13,16 @@ return {
     },
     filetypes = { "mma" },
     root_markers = { '.git' },
+    init_options = {
+        -- Needs explicitly enable the semantic tokens support.
+        -- Otherwise, there would be errors during initialization.
+        -- https://github.com/WolframResearch/LSPServer/blob/cecb4b310270d39fb7b
+        -- a05564e5d5ae89d27802d/LSPServer/Kernel/LSPServer.wl#L926
+        semanticTokens = true,
+        -- Displaying implicit tokens, and it is experimental.
+        -- Recommended settings from:
+        -- https://github.com/WolframResearch/LSPServer/blob/cecb4b310270d39fb7b
+        -- a05564e5d5ae89d27802d/docs/notes.md
+        implicitTokens = { "*", ",", ";;", "?" },
+    }
 }
